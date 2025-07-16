a1286b702f21f6ee37752eef2dc9561c82142d61
Liam French <lfrench@mun.ca>
2025-07-16 19:53:21 -0230

    Rename

--- RISC-V Core/run_all.sh
+++ RISC-V Core/run_all.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 cd python_if
-echo "Running assembler..."
+echo "----------------------------------------"
+echo "Running python assembler..."
+echo "----------------------------------------"
 python assembler.py
 mv instructions.mem ../instructions.mem
 cd ..
 cd sim
+echo "----------------------------------------"
 echo "Running simulation..."
-bash run.sh
+echo "----------------------------------------"
+if [ "$#" -eq 0 ]; then
+    bash run.sh
+elif [ "$1" == "gui" ]; then
+    bash run.sh gui
+else
+    echo "----------------------------------------"
+    echo "SCRIPT ERROR: Invalid argument. Use 'gui' for GUI mode or no argument for console mode."
+    echo "----------------------------------------"
+    exit 1
+fi
+echo "----------------------------------------"
 echo "Simulation complete. Moving log files..."
+echo "----------------------------------------"
 mv sim.log ../sim.log
 mv memory_write.log ../memory_write.log
--- RISC-V Core/sim/run.sh
+++ RISC-V Core/sim/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 rm -f vsim.wlf transcript *.vcd wlftjd*
 
-# Build
 vlib work
 vlog -sv +acc=rn ../components/*.sv ../components/*.vh
 
-# Simulate
-vsim -l sim.log -c -do sim.do
+if [ "$#" -eq 0 ]; then
+    vsim -l sim.log -c -do sim.do
+elif [ "$1" == "gui" ]; then
+    vsim -l sim.log -do sim.do
+else
+    echo "----------------------------------------"
+    echo "SCRIPT ERROR: Invalid argument. Use 'gui' for GUI mode or no argument for console mode."
+    echo "----------------------------------------"
+    exit 1
+fi
